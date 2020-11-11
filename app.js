d0d22d18dce3c9f1d9c5a2c14311b026c4e35789
arya28.id <arya28.id@gmail.com>
2020-11-11 14:50:20 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// var buah = "apple";
+	// switch (buah) {
+	// 	case "apple":
+	// 		alert("buahnya adalah apple");
+	// 		break;
+	// 	case "mangga":
+	// 		alert("buahnya adalah mangga");
+	// 		break;
+	// 	case "jambu":
+	// 		alert("buahnya adalah jambu");
+	// 		break;
+	// }
+
+	// for(i = 1; i <= 10; i++) {
+	// 	document.write(i);
+	// }
+		
+	// var i = 1;	
+	// while (i <= 5) {
+	// 	document.write("Nomor : "+i+"<br/>");
+	// 	i++;
+	// }
+
+	// // Fungsi
+	// function tes() {
+	// 	document.write("HELLO WORLD!");
+	// }
+	// //untuk menjalankan fungsi, cukup tulis namafungsi 
+	// tes();
+
+	// fungsi 2
+	// function kalikan(x, y) {
+	// 	z = x + y;
+	// 	document.write("Hasil kali "+x+" * "+y+" = "+z);
+	// }
+
+	// kalikan(5, 3);
+
+	// PENANGANAN EVENT
+
+	// function inform() {
+	// 	alert("Hai anda mengklik saya")
+    // }
+    var teks = "Muhammad Arya"
+
+    panjang = teks.length;
+
+    alert(panjang);
